1fdf813e36884f516690b7ccf2d6e63963fa03d8
kelumsampath <edirisingheemks@gmail.com>
2020-05-09 12:52:16 +0530

    cors allowed

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import express from "express";
 import { PORT } from "./config/constants";
 import { userRouter, eventRouter } from "./routes";
 
+var cors = require("cors");
+
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 app.use("/users", userRouter);
 app.use("/events", eventRouter);
 
