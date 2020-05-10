e53554400df3524806590583f2e549b38e730d84
kelumsampath <edirisingheemks@gmail.com>
2020-05-10 16:53:07 +0530

    swagger api documentation added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { PORT } from "./config/constants";
 import { userRouter, eventRouter } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
 
 var cors = require("cors");
 
 const app = express();
 app.use(express.json());
 
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.use("/users", userRouter);
 app.use("/events", eventRouter);
