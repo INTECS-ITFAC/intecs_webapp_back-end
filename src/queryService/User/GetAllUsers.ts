434253058ba7cba37cdb403672ff30dbcb8714b8
CharinduItFac <164178m@uom.lk>
2020-05-14 12:06:40 +0530

    filtered get all users response

--- src/queryService/User/GetAllUsers.ts
+++ src/queryService/User/GetAllUsers.ts
@@ -1,9 +1,12 @@
 import dbGetAll from "../../database/GetAll";
+import { user } from "../../models/UserTypes";
 
 const collectionName = "Users";
 
 const getAllUsers = (callback: any) => {
   dbGetAll(collectionName, (result: any) => {
+    let users: user[] = result;
+    users.map((user) => delete user.password);
     callback(result);
   });
 };
